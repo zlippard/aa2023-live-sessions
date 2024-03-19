ef6037ae52d7e8b967ecbd83c8671f3e6557e3de
Zac Lippard <zlippard@gmail.com>
2024-03-18 23:42:37 -0700

    MVI example

--- app/src/main/java/com/kodeco/android/aa2023/ui/screens/MainScreen.kt
+++ app/src/main/java/com/kodeco/android/aa2023/ui/screens/MainScreen.kt
@@ -1,32 +1,63 @@
 package com.kodeco.android.aa2023.ui.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.delay
 
 @Composable
 fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
+    onButtonClick: (() -> Unit)? = null,
+    onNavigate: ((route: String) -> Unit)? = null,
 ) {
     val backgroundPrimaryColor = MaterialTheme.colorScheme.background
     val backgroundSecondaryColor = MaterialTheme.colorScheme.secondary
     var backgroundColor by remember { mutableStateOf(backgroundPrimaryColor) }
+    val state by viewModel.stateFlow.collectAsState()
+    val context = LocalContext.current
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = backgroundColor
     ) {
-        Text("Hello, Android, here's the school data: ${viewModel.getSchool()}")
+        when (val currentState = state) {
+            is SchoolState.Loading -> Text("Loading")
+            is SchoolState.Loaded -> {
+                Column {
+                    Text("Hello, Android, here's the school data: ${currentState.school}")
+                    Button(onClick = { viewModel.sendIntent(Intent.DoSomething) })  {
+                        Text("Do something")
+                    }
+                }
+
+            }
+            is SchoolState.Error -> Text("Error")
+        }
+
+        LaunchedEffect(Unit) {
+            viewModel.actionFlow.collect { action ->
+                when (action) {
+                    is Action.Toast -> Toast.makeText(context, action.message, Toast.LENGTH_LONG).show()
+                    is Action.Navigate -> onNavigate?.invoke(action.route)
+                }
+                // Perform action
+            }
+        }
 
         LaunchedEffect(Unit) {
             delay(2_500L)
--- app/src/main/java/com/kodeco/android/aa2023/ui/screens/MainViewModel.kt
+++ app/src/main/java/com/kodeco/android/aa2023/ui/screens/MainViewModel.kt
@@ -1,14 +1,58 @@
 package com.kodeco.android.aa2023.ui.screens
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kodeco.android.aa2023.repositories.SchoolRepository
 import com.kodeco.android.aa2023.interop.School
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+sealed interface SchoolState {
+    data object Loading : SchoolState
+    data class Loaded(val school: School) : SchoolState
+    data object Error : SchoolState
+}
+
+sealed interface Action {
+    data class Toast(val message: String) : Action
+    data class Navigate(val route: String) : Action
+}
+
+sealed interface Intent {
+    data object DoSomething : Intent
+}
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: SchoolRepository,
 ): ViewModel() {
-    fun getSchool(): School = repository.getSchool()
+
+    private val _stateFlow = MutableStateFlow<SchoolState>(SchoolState.Loading)
+    val stateFlow: StateFlow<SchoolState> = _stateFlow.asStateFlow()
+
+    private val _actionFlow = MutableSharedFlow<Action>()
+    val actionFlow: SharedFlow<Action> = _actionFlow.asSharedFlow()
+
+    fun sendIntent(intent: Intent) {
+        when (intent) {
+            is Intent.DoSomething -> doSomething()
+        }
+    }
+
+    private fun doSomething() {
+        // Metrics / Analytics calls
+        viewModelScope.launch {
+            _actionFlow.emit(Action.Toast("Something happened"))
+            _actionFlow.emit(Action.Navigate("/details"))
+        }
+    }
+
+//    fun getSchool(): School = repository.getSchool()
 }
